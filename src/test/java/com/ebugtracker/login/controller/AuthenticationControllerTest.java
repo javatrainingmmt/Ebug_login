0533f8e6b04accbbd4dadc13b5318f58e34e36d4
mrudula <mrudulatri99@gmail.com>
2022-02-10 10:03:57 +0530

    commit

---
+++ src/test/java/com/ebugtracker/login/controller/AuthenticationControllerTest.java
@@ -0,0 +1,14 @@
+package com.ebugtracker.login.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AuthenticationControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
