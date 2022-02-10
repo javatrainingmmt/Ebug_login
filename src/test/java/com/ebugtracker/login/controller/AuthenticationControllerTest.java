67e5b38ff63c0d8f304dec9df5a97a71cb682b4f
mrudula <mrudulatri99@gmail.com>
2022-02-10 10:10:22 +0530

    test added

--- src/test/java/com/ebugtracker/login/controller/AuthenticationControllerTest.java
+++ src/test/java/com/ebugtracker/login/controller/AuthenticationControllerTest.java
@@ -3,12 +3,31 @@ package com.ebugtracker.login.controller;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ebugtracker.login.entity.UserEntity;
+import com.ebugtracker.login.repository.UserRepository;
 
 class AuthenticationControllerTest {
 
+	@Autowired
+	private AuthenticationController authc;
+	
+	@Autowired
+	private UserRepository userrepo;
+	
+	@Autowired
+	private UserEntity user;
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void register(){
+		UserEntity user = new UserEntity(1,"admin","password","ADMIN");
+		userrepo.save(user);
+		UserEntity user1= userrepo.findByUsername("admin");
+		assertNotNull(user);
+		assertEquals(user.getUsername(),user1.getUsername());
+		
+		
 	}
 
 }
